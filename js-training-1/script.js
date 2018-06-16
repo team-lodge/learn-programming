2d6381d7eace375c6bf6e0fd70ddea97d7f01d07
ryotsun <ryo.tsugawa@gmail.com>
2018-06-16 15:41:37 +0900

    簡易抽選システムをざっくり実装

---
+++ js-training-1/script.js
@@ -0,0 +1,52 @@
+window.onload = function () {
+  const submit_button = document.getElementById('submit');
+
+  /**
+   * フォーム内のボタンをイベントリスナーに登録し、クリックを監視
+   */
+  submit_button.addEventListener('click', function () {
+    const form = document.form1;
+    const names = form.names.value;
+    const orig_list = names.split(',');
+    let list = orig_list;
+
+    const count = form.num.value;
+    let winners = [];
+
+    // 指定された当選者数分だけ for で回す
+    for (let i = 0; i < count; i++) {
+      if (i > orig_list) {
+        break;
+      }
+      /**
+       * 乱数の取得に関して、このサイトがわかりやすいです。
+       * https://www.sejuku.net/blog/22432
+       */
+      const chosen_index = Math.floor(Math.random() * list.length);
+      // 当選者を当選者用配列に詰める
+      winners.push(list[chosen_index]);
+
+      // 応募者リストから、当選したメンバーを除外する
+      list.splice(chosen_index, 1);
+    }
+    output(winners);
+  });
+};
+
+/**
+ * HTML タグを生成して出力する
+ * @param winners array 当選者の配列
+ */
+function output(winners) {
+  let elem = document.getElementById('js-list');
+  elem.innerText = '';
+
+  let ul = document.createElement('ul');
+  for(let i = 0; i < winners.length; i++) {
+    let li = document.createElement('li');
+    li.innerText = winners[i];
+    ul.appendChild(li);
+  }
+
+  elem.appendChild(ul);
+}
