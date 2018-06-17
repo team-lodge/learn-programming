09f9900bfae616a75a66c4a7baec2f8a28a48a3c
ryotsun <ryo.tsugawa@gmail.com>
2018-06-18 00:18:43 +0900

    不具合修正とリファクタリング

--- js-training-1/script.js
+++ js-training-1/script.js
@@ -1,32 +1,36 @@
+/**
+ * メンバーを抽選する
+ */
 function chooseMember() {
   const form = document.form1;
   const names = form.names.value;
   const orig_list = names.split(',');
-  let list = orig_list;
+  // 値渡しにする
+  let list = orig_list.slice(0, orig_list.length);
 
   const count = form.num.value;
 
   let winners = [];
 
-  validation(list, count);
+  if (validation(list, count)) {
+    // 指定された当選者数分だけ for で回す
+    for (let i = 0; i < count; i++) {
+      if (i + 1 > orig_list.length) {
+        break;
+      }
+      /**
+       * 乱数の取得に関して、このサイトがわかりやすいです。
+       * https://www.sejuku.net/blog/22432
+       */
+      const chosen_index = Math.floor(Math.random() * list.length);
+      // 当選者を当選者用配列に詰める
+      winners.push(list[chosen_index]);
 
-  // 指定された当選者数分だけ for で回す
-  for (let i = 0; i < count; i++) {
-    if (i > orig_list) {
-      break;
+      // 応募者リストから、当選したメンバーを除外する
+      list.splice(chosen_index, 1);
     }
-    /**
-     * 乱数の取得に関して、このサイトがわかりやすいです。
-     * https://www.sejuku.net/blog/22432
-     */
-    const chosen_index = Math.floor(Math.random() * list.length);
-    // 当選者を当選者用配列に詰める
-    winners.push(list[chosen_index]);
-
-    // 応募者リストから、当選したメンバーを除外する
-    list.splice(chosen_index, 1);
+    output(winners);
   }
-  output(winners);
 }
 
 /**
@@ -51,6 +55,7 @@ function output(winners) {
  * バリデーション
  * @param list Array 応募者一覧
  * @param count Int 当選者数
+ * @returns {boolean}
  */
 function validation(list, count) {
   let alert = document.getElementById('alert');
@@ -70,4 +75,6 @@ function validation(list, count) {
 
     return false;
   }
+
+  return true;
 }
