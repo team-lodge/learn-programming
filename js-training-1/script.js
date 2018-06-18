ef404c5057ee7551d425295903de423dbf908c9b
ryotsun <ryo.tsugawa@gmail.com>
2018-06-18 21:57:20 +0900

    リファクタリング（続き）

--- js-training-1/script.js
+++ js-training-1/script.js
@@ -5,17 +5,19 @@ function chooseMember() {
   const form = document.form1;
   const names = form.names.value;
   const orig_list = names.split(',');
+  const base_list = removeEmptyStringValueFromArray(orig_list);
   // 値渡しにする
-  let list = orig_list.slice(0, orig_list.length);
+  let list = base_list.slice(0, base_list.length);
 
+  // フォーム内の不要なカンマを削除する
+  form.names.value = list.join(',');
   const count = form.num.value;
-
   let winners = [];
 
   if (validation(list, count)) {
     // 指定された当選者数分だけ for で回す
     for (let i = 0; i < count; i++) {
-      if (i + 1 > orig_list.length) {
+      if (i + 1 > base_list.length) {
         break;
       }
       /**
@@ -34,8 +36,25 @@ function chooseMember() {
 }
 
 /**
+ * 配列内にある空文字列の要素を削除する
+ * @param {string[]} array - 空文字列を含む配列
+ * @returns {string[]} list - 空文字列要素を削除した配列
+ */
+function removeEmptyStringValueFromArray(array) {
+  let list = [];
+
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (array[i] !== '' ) {
+      list.push(array[i]);
+    }
+  }
+
+  return list;
+}
+
+/**
  * HTML タグを生成して出力する
- * @param winners array 当選者の配列
+ * @param {string[]} winners 当選者の配列
  */
 function output(winners) {
   let elem = document.getElementById('js-list');
@@ -53,8 +72,8 @@ function output(winners) {
 
 /**
  * バリデーション
- * @param list Array 応募者一覧
- * @param count Int 当選者数
+ * @param {string[]} list 応募者一覧
+ * @param {number} count 当選者数
  * @returns {boolean}
  */
 function validation(list, count) {
