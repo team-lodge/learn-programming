dd8601a527d0de9c8664ee46ad2fba587c101120
testwata <testwata3san@gmail.com>
2018-10-02 00:03:45 +0900

    titleadd_2

--- js-training-2/team1/script.js
+++ js-training-2/team1/script.js
@@ -1,73 +1,62 @@
 /**
  * メンバーを抽選する
+ * participantList:参加者一覧
  */
 function chooseMember() {
-  /* formタグを取得 */
   const form = document.form1;
-  /* 参加者一覧を取得 */
-  const names = form.names.value;
-  /* 参加者一覧を配列にする */
-  const orig_list = names.split('\n');
-  /* 参加者一覧から空の要素をとる */
-  let list = removeEmptyStringValueFromArray(orig_list);
-  /* 参加者一覧をフォームに返す */
-  form.names.value = list.join('\n');
-  /* チーム数 */
-  const count = form.num.value;
-  /* チーム分け結果 */
-  let winners = [];
-  /* sub-titleの取得 */
+  const participant = form.participant.value;
+  const participantList = participant.split('\n');
+  let list = removeEmptyStringValueFromArray(participantList);
+  form.participant.value = list.join('\n');
+  const teamNum = form.team.value;
+  let resultList = [];
   const subtitle = document.getElementById('sub_title');
-  /* 参加者数 */
-  let participants_num = list.length;
-  /* チーム数・参加者数をサブタイトルに表示 */
-  subtitle.innerHTML = "参加者" + participants_num + "人中、" + count + "チームに分ける";
+  let participantsNum = list.length;
 
-  if (validation(list, count)) {
-    const group_array = getGroupMembers(count, list.length);
+  subtitle.innerText = "参加者" + participantsNum + "人中、" + teamNum + "チームに分ける";
+
+  if (validation(list, teamNum)) {
+    const groupArray = getGroupMembers(teamNum, participantsNum);
 
     // 指定されたチーム数分だけ for で回す
-    for (let i = 0; i < count; i++) {
+    for (let i = 0; i < teamNum; i++) {
       let members = [];
 
-      for (let j = 0; j < group_array[i]; j++ ) {
+      for (let j = 0; j < groupArray[i]; j++ ) {
         /**
          * 乱数の取得に関して、このサイトがわかりやすいです。
          * https://www.sejuku.net/blog/22432
          */
-        const chosen_index = Math.floor(Math.random() * list.length);
+
+        const chosenIndex = Math.floor(Math.random() * list.length);
         // 当選者を当選者用配列に詰める
-        members.push(list[chosen_index]);
-
+        members.push(list[chosenIndex]);
         // 参加者リストから、選んだメンバーを除外する
-        list.splice(chosen_index, 1);
+        list.splice(chosenIndex, 1);
       }
-
-      winners.push(members);
+      resultList.push(members);
     }
-    output(winners);
+    output(resultList);
   }
 }
 
 /**
  * チームごとの人数の配列を返す
- * @param {Number} group_num - チーム数
+ * @param {Number} teamNum - チーム数
  * @param {Number} total - 合計人数
  * @returns {Array} - チームごとの人数の配列
  */
-function getGroupMembers(group_num, total) {
-  let surplus = total % group_num;
-  let group_members = Math.floor(total / group_num);
+function getGroupMembers(teamNum, total) {
+  let surplus = total % teamNum;
+  let teamMembers = Math.floor(total / teamNum);
   let groups = [];
 
-  for (let i = 0; i < group_num; i++ ) {
-    groups.push(group_members);
-
+  for (let i = 0; i < teamNum; i++ ) {
+    groups.push(teamMembers);
   }
   for (let i = 0; i < surplus; i++ ) {
     groups[i] += 1;
   }
-
   return groups;
 }
 
@@ -84,60 +73,53 @@ function removeEmptyStringValueFromArray(array) {
       list.push(array[i]);
     }
   }
-
   return list;
 }
 
 /**
  * HTML タグを生成して出力する
- * @param {string[]} winners 当選者の配列
+ * @param {string[]} resultList 当選者の配列
  */
-function output(winners) {
+function output(resultList) {
   let elem = document.getElementById('js-list');
   elem.innerText = '';
-
   let ol = document.createElement('ol');
-  for(let i = 0; i < winners.length; i++) {
+  for (let i = 0; i < resultList.length; i++) {
     let li = document.createElement('li');
-    li.innerText = winners[i];
+    li.innerText = resultList[i];
     ol.appendChild(li);
   }
-
   elem.appendChild(ol);
 }
 
 /**
  * バリデーション
  * @param {string[]} list 応募者一覧
- * @param {number} count 当選者数
+ * @param {number} teamNum チーム数
  * @returns {boolean}
  */
-function validation(list, count) {
+function validation(list, teamNum) {
   let alert = document.getElementById('alert');
   alert.innerText = '';
   if (list.length === 0) {
     let li = document.createElement('li');
-    li.innerText = '応募者を入力してください';
+    li.innerText = '参加者を入力してください';
     alert.appendChild(li);
-
     return false;
   }
 
-  if (count < 1) {
+  if (teamNum < 1) {
     let li = document.createElement('li');
-    li.innerText = '当選者数を入力してください';
+    li.innerText = 'チーム数を入力してください';
     alert.appendChild(li);
-
     return false;
   }
 
-  if (list.length < count) {
+  if (list.length < teamNum) {
     let li = document.createElement('li');
     li.innerText = '適切なグループ数を入力してください';
     alert.appendChild(li);
-
     return false;
   }
-
   return true;
 }
